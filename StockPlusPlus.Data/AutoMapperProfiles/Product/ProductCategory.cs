5081b8bc55a3d4c45f6c33b3c4b2152ea43edd3f
Aza Asim <azaasim@gmail.com>
2023-09-27 17:55:35 +0300

    Remove ShiftFileDTO mapping on Product Category Mapping (It's now Implemented by default)

--- StockPlusPlus.Data/AutoMapperProfiles/Product/ProductCategory.cs
+++ StockPlusPlus.Data/AutoMapperProfiles/Product/ProductCategory.cs
@@ -1,8 +1,6 @@
 ﻿
 using AutoMapper;
-using ShiftSoftware.ShiftEntity.Model.Dtos;
 using StockPlusPlus.Shared.DTOs.Product.ProductCategory;
-using System.Text.Json;
 
 namespace StockPlusPlus.Data.AutoMapperProfiles.Product;
 
@@ -10,36 +8,8 @@ public class ProductCategory : Profile
 {
     public ProductCategory()
     {
-        CreateMap<Entities.Product.ProductCategory, ProductCategoryDTO>()
-            .ForMember(
-                dest => dest.Photos,
-                opt => opt.MapFrom(src => deserialize(src.Photos))
-            )
-            .ReverseMap().ForMember(
-                dest => dest.Photos,
-                opt => opt.MapFrom(src => Serialize(src.Photos))
-            );
+        CreateMap<Entities.Product.ProductCategory, ProductCategoryDTO>().ReverseMap();
 
         CreateMap<Entities.Product.ProductCategory, ProductCategoryListDTO>();
-    }
-
-    private string? Serialize(List<ShiftFileDTO>? shiftFileDTOs)
-    {
-        if (shiftFileDTOs == null)
-            return null;
-
-        shiftFileDTOs.ForEach(x => x.Url = null);
-
-        return JsonSerializer.Serialize(shiftFileDTOs);
-    }
-
-    private List<ShiftFileDTO> deserialize(string? text)
-    {
-        if (string.IsNullOrWhiteSpace(text))
-        {
-            return new List<ShiftFileDTO>();
-        }
-
-        return JsonSerializer.Deserialize<List<ShiftFileDTO>>(text) ?? new List<ShiftFileDTO>();
     }
 }
