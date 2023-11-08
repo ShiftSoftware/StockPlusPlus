b1282cd8962f1414088417913210017ac4ea55a7
AzaAsim <aza@shift.software>
2023-11-09 01:02:39 +0300

    Setup Tests

---
+++ StockPlusPlus.Test/CustomWebApplicationFactory.cs
@@ -0,0 +1,32 @@
+﻿
+using ShiftSoftware.ShiftFrameworkTestingTools;
+using StockPlusPlus.API;
+using StockPlusPlus.Data;
+using StockPlusPlus.Shared.ActionTrees;
+
+namespace StockPlusPlus.Test;
+
+public class CustomWebApplicationFactory : ShiftCustomWebApplicationFactory<WebMarker, DB>
+{
+    public CustomWebApplicationFactory() : base(
+        "SQLServer_Test",
+        new ShiftCustomWebApplicationBearerAuthSettings
+        {
+            Enabled = true,
+            TokenKeySettingKey = "Settings:TokenSettings:Key",
+            TokenIssuerSettingKey = "Settings:TokenSettings:Issuer",
+            TypeAuthActions = new List<Type>()
+            {
+                typeof(StockActionTrees)
+            }
+        })
+    { }
+}
+
+[CollectionDefinition("API Collection")]
+public class APICollection : ICollectionFixture<CustomWebApplicationFactory>
+{
+    // This class has no code, and is never created. Its purpose is simply
+    // to be the place to apply [CollectionDefinition] and all the
+    // ICollectionFixture<> interfaces.
+}
