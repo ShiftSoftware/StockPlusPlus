d11ff911c0cd24b3cae6fb840f6fec03ebbb12ea
AzaAsim <aza@shift.software>
2024-01-11 01:51:55 +0300

    Add Category Printing (Data & API only)

--- StockPlusPlus.API/Controllers/Product/ProductCategoryController.cs
+++ StockPlusPlus.API/Controllers/Product/ProductCategoryController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using ShiftSoftware.ShiftEntity.Web;
 using StockPlusPlus.Data.Repositories.Product;
 using StockPlusPlus.Shared.ActionTrees;
@@ -9,12 +10,20 @@ namespace StockPlusPlus.API.Controllers.Product;
 [Route("api/[controller]")]
 public class ProductCategoryController : ShiftEntitySecureControllerAsync<ProductCategoryRepository, Data.Entities.Product.ProductCategory, ProductCategoryListDTO, ProductCategoryDTO>
 {
-    public ProductCategoryController() : base(StockActionTrees.ProductCategory, x =>
+    private readonly ProductCategoryRepository repository;
+    public ProductCategoryController(ProductCategoryRepository repository) : base(StockActionTrees.ProductCategory, x =>
         x.FilterBy(x => x.ID, StockActionTrees.DataLevelAccess.ProductCategory)
         .DecodeHashId<_ProductCategoryHashId>()
         .IncludeCreatedByCurrentUser(x => x.CreatedByUserID)
     )
     {
+        this.repository = repository;
+    }
 
+    [HttpGet("print/{ID}")]
+    [AllowAnonymous]
+    public async Task<ActionResult> Print(string id)
+    {
+        return new FileStreamResult(await this.repository.Print(id), "application/pdf");
     }
 }
